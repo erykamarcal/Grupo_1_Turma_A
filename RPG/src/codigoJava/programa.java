6f44e1f2b3f8a50fb3a4d4df39ade51a6209d39a
Alice Harder <alice.harder2007@gmail.com>
2025-11-09 14:08:48 -0300

    adicionando efeito de cor

--- RPG/src/codigoJava/programa.java
+++ RPG/src/codigoJava/programa.java
@@ -291,6 +291,21 @@ public class programa {
         }
         System.out.println();
     }
+
+    public static class cores{
+        public static final String RESET ="\u001B[0m";
+        public static final String VERMELHO ="\u001B[31m";
+        public static final String VERDE ="\u001B[32m";
+        public static final String AMARELO ="\u001B[33m";
+        public static final String AZUL ="\u001B[34m";
+        public static final String ROXO ="\u001B[35m";
+        public static final String CIANO ="\u001B[36m";
+
+        public static void fala (String cor, String texto){
+            System.out.println(cor + texto + RESET);
+        }
+    }
+
 // ==============FIM EFEITOS DE TEXTO===============
 
 // ============ INICIO FUNCOES DE TUTORIAIS================
